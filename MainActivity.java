73a6451e13e398a4c26fcb97cb5de3ca07914f08
Gopalsorout <51239853+Gopalsorout@users.noreply.github.com>
2019-06-12 09:35:02 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,68 +1,177 @@
-package com.aptron.internalst;
+package com.example.vaibhav.multithreading;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+ TextView text1,text2;
+ Button btn1;
+ ProgressBar progressBar;
+ ListView list1;
+ LinearLayout mylayout;
 
-    EditText editText,editText2;
-    Button btn,btn1;
+    private static final String TAG = "abhi";
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.editText);
-
-        editText2 = findViewById(R.id.editText1);
-        btn=findViewById(R.id.btn);
+        text1=findViewById(R.id.text1);
+        text2=findViewById(R.id.text2);
         btn1=findViewById(R.id.btn1);
+        list1=findViewById(R.id.list1);
+        mylayout = findViewById(R.id.linear);
+       final String[] img_url = getResources().getStringArray(R.array.image_url);
 
+        list1.setAdapter(new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,getResources().getStringArray(R.array.image)));
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                String email = editText.getText().toString();
-                String pass = editText2.getText().toString();
-                FileOutputStream stream = null;
+            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
 
-                email = email+",";
-
-                try {
-                    stream =openFileOutput("abhi.txt",MODE_PRIVATE);
-                    stream.write(email.getBytes());
-                    stream.write(pass.getBytes());
-
-                    Toast.makeText(MainActivity.this, "Data Save SuccessFully", Toast.LENGTH_SHORT).show();
-
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, "Exception !!!", Toast.LENGTH_SHORT).show();
-                }
-                finally {
-                    try {
-                        stream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                text1.setText(img_url[position]);
             }
         });
+
+        isReadStoragePermissionGranted();
+        isWriteStoragePermissionGranted();
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            public void onClick(View v)
+            {
+          Thread thread =new Thread(new MyThread());
+          thread.start();
 
             }
         });
+
+    }
+
+    public void downloadImage(String url)
+    {
+        HttpURLConnection connection= null;
+        InputStream inputStream = null;
+        FileOutputStream outputStream = null;
+        try {
+            URL myurl = new URL(url);
+          connection = (HttpURLConnection) myurl.openConnection();
+      inputStream = connection.getInputStream();
+            File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+           Uri uri= Uri.parse(url);
+            File myFile = new File(folder,uri.getLastPathSegment());
+           outputStream = new FileOutputStream(myFile);
+            int read ;
+           byte[] arr = new byte[1024];
+            while ((read=inputStream.read(arr))!=-1)
+            {
+                outputStream.write(arr,0,read);
+            }
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mylayout.setVisibility(View.GONE);
+                }
+            });
+            //Toast.makeText(this, "Data Save Successfully", Toast.LENGTH_SHORT).show();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+          connection.disconnect();
+            try {
+                inputStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+    }
+
+    class MyThread implements Runnable
+    {
+        @Override
+        public void run() {
+
+         MainActivity.this.runOnUiThread(new Runnable() {
+             @Override
+             public void run() {
+                 mylayout.setVisibility(View.VISIBLE);
+             }
+         });
+            downloadImage(text1.getText().toString());
+        }
+    }
+
+    public  boolean isReadStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted1");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked1");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted1");
+            return true;
+        }
+    }
+
+    public  boolean isWriteStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted2");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked2");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted2");
+            return true;
+        }
     }
 }
